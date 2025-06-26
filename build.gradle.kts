1d12772f8bfbc3407cb345b7a9efcd3405f4043c
Stuart MacCallum <maccalsa@googlemail.com>
2025-06-26 11:35:32 +0100

    Add publishing plugon and pebble for templating

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,47 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    `maven-publish`
 }
 
-group = "com.logimos.filekit"
+group = "com.logimos"
 version = "1.0.0"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("io.pebbletemplates:pebble:3.2.2")
     testImplementation(kotlin("test"))
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = group.toString()
+            artifactId = name
+            version = version
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/logimos/filekit")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
